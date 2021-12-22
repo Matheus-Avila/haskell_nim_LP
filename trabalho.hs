c71b43ab6923ecbd2ca238f6e430a794d14c1964
Matheus-Avila <matheusavila1997@gmail.com>
2021-12-22 13:36:46 -0300

    Player sozinho funcionando

--- trabalho.hs
+++ trabalho.hs
@@ -13,10 +13,16 @@ import Data.Array.IO
 strToInt x = read x :: Int
 intToStr x = show x :: String
 interface = do
-    let linha0Str = palitosStr !! 0
-    let linha1Str = palitosStr !! 1
-    let linha2Str = palitosStr !! 2
-    let linha3Str = palitosStr !! 3
+    read <- readFile "palitos.txt"
+    palitosArr <- rList read
+    let linha0Int = palitosArr !! 0
+    let linha1Int = palitosArr !! 1
+    let linha2Int = palitosArr !! 2
+    let linha3Int = palitosArr !! 3
+    let linha0Str = intToStr linha0Int
+    let linha1Str = intToStr linha1Int
+    let linha2Str = intToStr linha2Int
+    let linha3Str = intToStr linha3Int
     putStrLn "Configuração dos palitos:"
     putStrLn ("0: " ++ linha0Str)
     putStrLn ("1: " ++ linha1Str)
@@ -25,39 +31,77 @@ interface = do
     putStrLn "Escolha uma linha para remover os palitos:"
     linha <- getLine
     let linhaInt = strToInt linha
-    let valStr = palitosStr !! linhaInt
-    let numPalitosLinha = strToInt valStr
+    let numPalitosLinha = palitosArr !! linhaInt
+    let valStr = intToStr numPalitosLinha
     putStrLn ("Escolha quantos palitos quer remover. O maximo eh :" ++ valStr)
     qtdePalitos <- getLine
     let qtdePalitosInt = strToInt qtdePalitos
     let rest = numPalitosLinha - qtdePalitosInt
     let restStr = intToStr rest
     putStrLn ("rest:"++restStr)
-    let palitosStr = ["1", "3", restStr, "7"] in do
-    putStrLn "Configuração inicial:"
-    putStrLn ("0: " ++ head palitosStr)
-    putStrLn ("1: " ++ palitosStr !! 1)
-    putStrLn ("2: " ++ palitosStr !! 2)
-    putStrLn ("3: " ++ palitosStr !! 3)
-    if maximum palitosStr /= "0"
-    then do
+    if linhaInt == 0 
+        then do 
+            let linha0Int = rest
+            let linha0Str = intToStr linha0Int
+            let linha1Str = intToStr linha1Int
+            let linha2Str = intToStr linha2Int
+            let linha3Str = intToStr linha3Int
+            y <- writeFile "palitos.txt" ("[" ++ linha0Str ++ ","++ linha1Str ++ ","++ linha2Str ++ ","++ linha3Str ++ "]")
+            print rest
+    else print "linha0" 
+    if linhaInt == 1
+        then do
+            let linha1Int = rest
+            let linha0Str = intToStr linha0Int
+            let linha1Str = intToStr linha1Int
+            let linha2Str = intToStr linha2Int
+            let linha3Str = intToStr linha3Int
+            y <- writeFile "palitos.txt" ("[" ++ linha0Str ++ ","++ linha1Str ++ ","++ linha2Str ++ ","++ linha3Str ++ "]")
+            print rest
+    else print "linha1"
+    if linhaInt == 2
+        then do
+            let linha2Int = rest
+            let linha0Str = intToStr linha0Int
+            let linha1Str = intToStr linha1Int
+            let linha2Str = intToStr linha2Int
+            let linha3Str = intToStr linha3Int
+            y <- writeFile "palitos.txt" ("[" ++ linha0Str ++ ","++ linha1Str ++ ","++ linha2Str ++ ","++ linha3Str ++ "]")
+            print rest
+    else print "linha2"
+    if linhaInt == 3
+        then do 
+        let linha3Int = rest
+        let linha0Str = intToStr linha0Int
+        let linha1Str = intToStr linha1Int
+        let linha2Str = intToStr linha2Int
+        let linha3Str = intToStr linha3Int
+        print linha3Str
+        y <- writeFile "palitos.txt" ("[" ++ linha0Str ++ ","++ linha1Str ++ ","++ linha2Str ++ ","++ linha3Str ++ "]")
+--     let linha3Int = rest
+        print rest
+    else print "linha3"
+    novoRead <- readFile "palitos.txt"
+    novoPalitosArr <- rList novoRead
+    let max = maximum novoPalitosArr
+    let maxStr = intToStr max
+    print ("x: " ++ maxStr)
+    if maximum novoPalitosArr /= 0
+    then do 
+        putStrLn "String"
         interface
     else
         putStrLn "Acavou!!"
 
-palitosStr = ["1", "3", "5", "7"]
-main  = do {
+main = do {
     putStrLn "Escolha um modo de jogo";
     escolha <- getLine;
     putStrLn ("Voce escolheu " ++ escolha);
     if escolha == "1"
-        then  do
-    if maximum palitosStr /= "0"
-    then do 
+        then
         interface;
-        else do putStrLn ("Acabou!")
-        else do
-        putStrLn ("Acabou!");
+    else do
+    putStrLn ("Acabou!");
 }
 -- main = do
 --     putStrLn "Escolha um modo de jogo"
@@ -79,23 +123,25 @@ main  = do {
 --             putStrLn ("0: " ++ palitos !! 0)
 --             putStrLn ("1: " ++ palitos !! 1)
 
-data SomeData = SomeData { array :: IOArray Int Int }
+-- data Palitos = Palitos { array :: IOArray Int Int }
+
+
+-- menu = do
+--     arr <- newArray (1,4) 1
+--     let d = Palitos { array = arr }
+--     writeArray (array d) 2 3
+--     writeArray (array d) 3 5
+--     writeArray (array d) 4 7
+--     a <- readArray (array d) 3
+--     b <- readArray (array d) 4
+--     print (a,b)
+
 
 menu = do
-  arr <- newArray (1,4) 1
-  let d = SomeData { array = arr }
-  writeArray (array d) 2 3
-  writeArray (array d) 3 5
-  writeArray (array d) 4 7
-  a <- readArray (array d) 3
-  b <- readArray (array d) 4
-  print (a,b)
-
--- data SomeData = SomeData { array :: IOArray Int Float }
-
--- amenu = do 
---     arr <- newArray (1,) 1
---     a <- readArray arr 1
---     writeArray arr 1 64
---     b <- readArray arr 1 
---     print (a,b)
+    x <- readFile "palitos.txt"
+    palitos <- rList x
+    print (palitos !! 0)
+    y <- writeFile "palitos.txt" "[1,3,0,7]"
+    print palitos
+rList :: String -> IO [Int]	  
+rList = readIO
